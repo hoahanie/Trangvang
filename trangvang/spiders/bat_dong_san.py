126b7cf57beaadb6c62554cc720fb41c83410336
hoahanie <hoale160799@gmail.com>
2020-10-29 23:15:33 +0700

    done

--- trangvang/spiders/bat_dong_san.py
+++ trangvang/spiders/bat_dong_san.py
@@ -22,6 +22,28 @@ class bat_dong_san(scrapy.Spider):
             )
     def parse_page(self,response):
         urls = response.xpath('.//h2[@class="company_name"]//a/@href').extract()
+        for url in urls:
+            yield scrapy.Request(
+                url=url,
+                headers = self.headers,
+                callback = self.parse_detail
+            )
+
+    def parse_detail(self,response):
+        name = response.xpath('//div[@class="tencongty"]/h1/text()').get()
+        email = response.xpath('//div[@class="text_email"]/p/a/text()').get()
+        website = response.xpath('//div[@class="text_website"]/p/a/text()').get()
+        address = response.xpath('//div[@class="diachi_chitiet_li2dc"]/p/text()').get()
+        phone = response.xpath('//div[@class="diachi_chitiet_li2"]/span/text()').get()
+        thitruong = response.xpath('//div[@class="thitruong_loaidn_text"]/p/text()').get()
+        loaihinh = response.xpath('//*[@id="listing_basic_info"]/div[5]/div[2]/div[2]/p/text()').get()
+
         yield {
-            "url": urls
+            "name": name,
+            "email": email,
+            "website": website,
+            "address": address,
+            "phone": phone,
+            "thitruong": thitruong,
+            "loaihinh": loaihinh
         }
--- trangvang/spiders/nha_thau_bds.py
+++ trangvang/spiders/nha_thau_bds.py
@@ -4,7 +4,7 @@ from scrapy.crawler import CrawlerProcess
 class nha_thau_bds(scrapy.Spider):
     name = 'nha_thau_bds'
 
-    start_urls = ['https://trangvangvietnam.com/categories/197610/bat-dong-san-nha-thau-bat-dong-san.html?']
+    start_urls = 'https://trangvangvietnam.com/categories/197610/bat-dong-san-nha-thau-bat-dong-san.html?'
 
     headers = {
         'user_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
@@ -16,12 +16,34 @@ class nha_thau_bds(scrapy.Spider):
 
         for i in range(1,int(number_page)+1):
             yield scrapy.Request(
-                url=self.start_urls[0] + "page=" + str(i),
+                url=self.start_urls + "page=" + str(i),
                 headers= self.headers,
                 callback=self.parse_page
             )
     def parse_page(self,response):
         urls = response.xpath('.//h2[@class="company_name"]//a/@href').extract()
+        for url in urls:
+            yield scrapy.Request(
+                url=url,
+                headers = self.headers,
+                callback = self.parse_detail
+            )
+
+    def parse_detail(self,response):
+        name = response.xpath('//div[@class="tencongty"]/h1/text()').get()
+        email = response.xpath('//div[@class="text_email"]/p/a/text()').get()
+        website = response.xpath('//div[@class="text_website"]/p/a/text()').get()
+        address = response.xpath('//div[@class="diachi_chitiet_li2dc"]/p/text()').get()
+        phone = response.xpath('//div[@class="diachi_chitiet_li2"]/span/text()').get()
+        thitruong = response.xpath('//div[@class="thitruong_loaidn_text"]/p/text()').get()
+        loaihinh = response.xpath('//*[@id="listing_basic_info"]/div[5]/div[2]/div[2]/p/text()').get()
+
         yield {
-            "url": urls
+            "name": name,
+            "email": email,
+            "website": website,
+            "address": address,
+            "phone": phone,
+            "thitruong": thitruong,
+            "loaihinh": loaihinh
         }
